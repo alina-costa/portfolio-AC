f87b68d68ef478fafd6eab87ef522d80d91e7a59
Alina Costa <alinacosta@Alinas-MBP-2.lan>
2022-10-27 23:59:19 -0400

    added resume file and target _blank

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
 // create a function to open each project website upon clicking the projects card
 
 // create event listeners
+let scrollFX = document.getElementsByClassName("scroll");
+
+// create an array from li elements
+// add an event listener using forEach?
+
+function scrollFunction2() {
+  let e = document.getElementById("el2");
+  // This ends the block to the window
+  // bottom and also aligns the view to the center
+  e.scrollIntoView({
+    block: "end",
+    behavior: "smooth",
+    inline: "center",
+  });
+}
