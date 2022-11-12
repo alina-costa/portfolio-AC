575c1ea8fd880595ef1286ac6f1bdc87455bbd37
Alina Costa <alinacosta@Alinas-MacBook-Pro-2.local>
2022-11-12 14:19:16 -0500

    added technologies and links to project cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,4 @@
 // create event listeners
 let scrollFX = document.getElementsByClassName("scroll");
 
-// create an array from li elements
-// add an event listener using forEach?
-
-function scrollFunction2() {
-  let e = document.getElementById("el2");
-  // This ends the block to the window
-  // bottom and also aligns the view to the center
-  e.scrollIntoView({
-    block: "end",
-    behavior: "smooth",
-    inline: "center",
-  });
-}
+// create flip cards for projects with details on the back
